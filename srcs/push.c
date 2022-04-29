627ca85ab9e249666158b6d3ced38d602511c1f3
Meryem COURTOIS <mcourtoi@e3r6p19.clusters.42paris.fr>
2022-04-29 21:03:42 +0200

    UESH JVE KEN TA MAMAN

---
+++ srcs/push.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcourtoi <mcourtoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/15 12:52:18 by mcourtoi          #+#    #+#             */
+/*   Updated: 2022/04/27 04:03:36 by mcourtoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_pb(t_stack a, t_stack b, int c)
+{
+	if (a.arr)
+	{
+		b.arr[*b.size] = 0;
+		*b.size = *b.size + 1;
+		ft_rrb(b.arr, *b.size, 1);
+		b.arr[0] = a.arr[0];
+		ft_ra(a.arr, *a.size, 1);
+		*a.size = *a.size - 1;
+		a.arr[*a.size] = 0;
+		if (c == 0)
+			write(1, "pb\n", 3);
+	}
+}
+
+void	ft_pa(t_stack b, t_stack a, int c)
+{
+	if (b.arr)
+	{
+		a.arr[*a.size] = 0;
+		*a.size = *a.size + 1;
+		ft_rra(a.arr, *a.size, 1);
+		a.arr[0] = b.arr[0];
+		ft_rb(b.arr, *b.size, 1);
+		*b.size = *b.size - 1;
+		b.arr[*b.size] = 0;
+		if (c == 0)
+			write(1, "pa\n", 3);
+	}
+}
